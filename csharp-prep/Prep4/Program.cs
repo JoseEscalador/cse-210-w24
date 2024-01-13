5b85bb8ad441d098e36ec885a0ef0d7d920a6c28
Luís José <luisjpinheiro@gmail.com>
2024-01-13 05:20:38 -0300

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers and type 0 when finished.");
+        int number = 1;
+        List<int> numbers = new List<int>(); 
+        float total = 0;
+        int maxNumber = 0;
+        while (number != 0) 
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+            total = total + number;
+            if (maxNumber < number){
+                maxNumber = number;
+            }
+        }
+        float avg = total /(numbers.Count - 1);
+        Console.WriteLine($"The sum is {total}");
+        
+        Console.WriteLine($"The average is: {avg}");
+        Console.WriteLine($"The largest number is {maxNumber}");
+        
+
     }
 }
