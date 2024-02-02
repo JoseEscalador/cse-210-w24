d91c50f515ad50461b224c3228cc81e6a7847d89
Luís José <luisjpinheiro@gmail.com>
2024-02-02 17:14:09 -0300

    w4 Learning commit

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment newAssignment = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(newAssignment.GetSummary());
+
+        MathAssignment newMath = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(newMath.GetSummary());
+        Console.WriteLine(newMath.GetHomeWork());
+        WritingAssigment newWriting = new WritingAssigment("Mary Waters", "European History", "The Causes of World War");
+        Console.WriteLine(newWriting.GetSummary());
+        Console.WriteLine(newWriting.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssigment.cs
@@ -0,0 +1,12 @@
+public class WritingAssigment : Assignment {
+    private string _title;
+
+    public WritingAssigment (string studentName, string topic, string title):base(studentName, topic) {
+        _title = title;
+    }
+
+    public string GetWritingInformation() {
+        return $"{_title} by {GetStudentName()}";
+        
+    }
+}
